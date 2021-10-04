b8e63e2b09ef883b772f1bf5f627438beb3e2c49
najmus-sakib <najmussakib@iut.edu>
2021-10-04 22:05:49 +0600

    newuserwindow

--- classroom/Views/NewUser.xaml.cs
+++ classroom/Views/NewUser.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace classroom
+namespace classroom.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NewUser.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NewUser : Window
     {
-        public MainWindow()
+        public NewUser()
         {
             InitializeComponent();
-            
         }
     }
 }
