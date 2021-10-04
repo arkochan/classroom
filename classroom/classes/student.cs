a9994ca75f212cbc13354286098871890ba37ca1
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2021-10-04 21:54:33 +0600

    added basic structure --> student , user

--- classroom/classes/student.cs
+++ classroom/classes/student.cs
@@ -8,6 +8,34 @@ namespace classroom.classes
 {
     class student: user
     {
-        int a;
+        private List<string> enrolledCourses = new List<string>();
+
+        public void addCourses(string courseID) // on review
+        {
+
+        }
+
+        public student(string _name, string _password, string _email, string _phone) : base(_name, _password, _email, _phone)
+        {
+            generateUniqueID();
+        }
+        ~student() { }
+
+        protected override void generateUniqueID()
+        {
+            uniqueID = "stu" + email;
+        }
+        public override bool sendMessages(string msg, string _userName)
+        {
+            // search teacher username in db
+            // send the string to the teacher's msg section
+            return true;
+        }
+
+        public bool feedback(string _feedback)
+        {
+            // send feedback to the course section
+            return true;
+        }
     }
 }
--- classroom/classes/user.cs
+++ classroom/classes/user.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace classroom.classes
 {
-    class user
+    abstract class user
     {
-        int a;
+
+        protected string department { get; set; }
+        protected int age {  get; set;  }  // review
+        protected string email { get; set; }
+        protected string password { get; set; }
+        protected string phone { get; set; }
+        protected string name { get; set; }
+        protected string notification { get; set; }
+        protected string messeges { get; set; }
+        protected int role { get; set; }
+        
+        protected string uniqueID;
+
+        protected user(string _name, string _password, string _email, string _phone)
+        {
+            name = _name;
+            password = _password;
+            email = _email;
+            phone = _phone;
+        }
+
+        ~user() { }
+
+        public abstract bool sendMessages(string msg, string _userName);
+        protected abstract void generateUniqueID();
     }
 }
