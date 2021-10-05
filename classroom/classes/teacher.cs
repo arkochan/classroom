b132b6633971bfb859f1a2c2f96b7f5be360f701
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2021-10-05 15:40:25 +0600

    day 2 15:40

---
+++ classroom/classes/teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classroom.classes
+{
+    class teacher:user
+    {
+        List<course> enrolledCourses = new List<course>();
+        List<student> tracker = new List<student>();
+
+    }
+}
